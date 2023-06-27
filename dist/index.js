228b3922d7f88e5a388ca6477f7c7e492675ec6c
Tyler Underwood <twood9600@yahoo.com>
2023-06-27 18:37:23 -0400

    made a function

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,42 @@
 const inquirer = require('inquirer')
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
+const { Triangle, Square, Circle } = require("./lib");
 // const fs = require('fs');
 // TODO: Create an array of questions for user input
 // ADD VALIDATION TO QUESTIONS!!!!!!!
+function writeToFile(fileName, answers) {
+    // File is an empty string
+    let svgString = "";
+    //set the height and width of logo container
+    svgString =
+      '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    
+    svgString += "<g>";
+   
+    svgString += `${answers.shape}`;
+let shapeChoice;
+  if (answers.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
+    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+  } else {
+    shapeChoice = new Circle();
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+  }
+  
+  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+
+  svgString += "</g>";
+
+  svgString += "</svg>";
+
+  fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log("Generated logo.svg");
+  });
+}
 inquirer.prompt([
     {
         type: 'maxLength-input',
@@ -19,7 +52,7 @@ inquirer.prompt([
              }
          }
      },{
-     type: 'maxLength-input',
+     type: 'input',
         name: 'color',
         message: 'What color would you like?',
         validate: colorInput => {
@@ -31,7 +64,7 @@ inquirer.prompt([
              }
          }
      },{
-        type: 'maxLength-input',
+        type: 'input',
         name: 'shape',
         message: 'What shape would you like?',
         choices: 'square, circle, triangle',
